110193391eab995b657693ac73e78c93c370b4aa
harvest0623 <3367741939@qq.com>
2025-12-11 00:01:02 +0800

    hooks

--- React/hooks/src/AppClass.jsx
+++ React/hooks/src/AppClass.jsx
@@ -3,13 +3,11 @@ import React, { Component } from 'react'
 export default class App extends Component {
     constructor() {
         console.log('constructor');
-        super()
-
+        super();
         this.state = {
-        count: 0
+            count: 0
         }
     }
-
     componentDidMount() {
         console.log('组件加载完毕');
     }
@@ -21,23 +19,20 @@ export default class App extends Component {
     componentWillUnmount() {
         
     }
-
     add() {
         // this.state.count++
         // console.log(this.state.count);
         this.setState({
-        count: this.state.count + 1
+            count: this.state.count + 1
         })
     }
-
     render() {
         console.log('render');
-        
         return (
-        <div>
-            <h2>{this.state.count}</h2>
-            <button onClick={this.add.bind(this)}>add</button>
-        </div>
+            <div>
+                <h2>{this.state.count}</h2>
+                <button onClick={this.add.bind(this)}>add</button>
+            </div>
         )
     }
 }
