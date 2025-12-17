44b5132f8e72ad4a1750f5e26c584f1d02db0aab
harvest0623 <3367741939@qq.com>
2025-12-17 11:36:24 +0800

    hooks

--- React/hooks/src/main.jsx
+++ React/hooks/src/main.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
-import App from './App2'
+// import App from './App2'
 // import App from './AppClass'
-// import App from './App3'
+import App from './App3'
 
 createRoot(document.getElementById('root')).render(
     <App />
